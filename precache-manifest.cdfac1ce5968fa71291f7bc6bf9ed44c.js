f299775ebf1ef37be3d97c9d3324b4392ec7da44
gregorytcheng <gregorytcheng@gmail.com>
2020-06-22 19:26:07 -0700

    Updates

--- precache-manifest.cdfac1ce5968fa71291f7bc6bf9ed44c.js
+++ precache-manifest.cdfac1ce5968fa71291f7bc6bf9ed44c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46bbc30711a718981b3e0251dd15c2a7",
+    "revision": "bf3d039843bf89605300d54718b70d0d",
     "url": "/pathfinding-visualizer/index.html"
   },
   {
-    "revision": "aba792190a43a29e9bdb",
+    "revision": "05daeb2abd6ef0be40d6",
     "url": "/pathfinding-visualizer/static/css/main.9818aa58.chunk.css"
   },
   {
-    "revision": "fadcacfa6af245f4224c",
-    "url": "/pathfinding-visualizer/static/js/2.f0f599ad.chunk.js"
+    "revision": "ace0b363ff7f6f2a81e7",
+    "url": "/pathfinding-visualizer/static/js/2.4a626049.chunk.js"
   },
   {
     "revision": "4d47ffde3dc69784e8e23fd910817ef8",
-    "url": "/pathfinding-visualizer/static/js/2.f0f599ad.chunk.js.LICENSE.txt"
+    "url": "/pathfinding-visualizer/static/js/2.4a626049.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aba792190a43a29e9bdb",
-    "url": "/pathfinding-visualizer/static/js/main.c9f42bdf.chunk.js"
+    "revision": "05daeb2abd6ef0be40d6",
+    "url": "/pathfinding-visualizer/static/js/main.56fe4b09.chunk.js"
   },
   {
     "revision": "4b0188db55b92857fd33",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pathfinding-visualizer/precache-manifest.f8a4ab6818e159937834e7e85bbc6874.js"
+  "/pathfinding-visualizer/precache-manifest.cdfac1ce5968fa71291f7bc6bf9ed44c.js"
 );
 
 self.addEventListener('message', (event) => {
